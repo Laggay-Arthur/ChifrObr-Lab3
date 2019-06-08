398605d985cb74edf16c964d99d77348ef5edde2
Laggay-Arthur <39773470+Laggay-Arthur@users.noreply.github.com>
2019-06-08 11:25:59 +0300

    Lab3

---
+++ chiftObr_Lab3/chiftObr_Lab3/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace chiftObr_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Calculating = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Emax_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ti_input = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.P_input = new System.Windows.Forms.TextBox();
+            this.wmax = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Calculating
+            // 
+            this.Calculating.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Calculating.ForeColor = System.Drawing.Color.Gray;
+            this.Calculating.Location = new System.Drawing.Point(7, 390);
+            this.Calculating.Name = "Calculating";
+            this.Calculating.Size = new System.Drawing.Size(120, 36);
+            this.Calculating.TabIndex = 0;
+            this.Calculating.Text = "Выполнить";
+            this.Calculating.UseVisualStyleBackColor = true;
+            this.Calculating.Click += new System.EventHandler(this.Calculating_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Emax";
+            // 
+            // Emax_input
+            // 
+            this.Emax_input.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Emax_input.Location = new System.Drawing.Point(63, 7);
+            this.Emax_input.Name = "Emax_input";
+            this.Emax_input.Size = new System.Drawing.Size(64, 36);
+            this.Emax_input.TabIndex = 2;
+            this.Emax_input.Text = "14";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(4, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ti";
+            // 
+            // ti_input
+            // 
+            this.ti_input.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ti_input.Location = new System.Drawing.Point(63, 53);
+            this.ti_input.Name = "ti_input";
+            this.ti_input.Size = new System.Drawing.Size(64, 36);
+            this.ti_input.TabIndex = 2;
+            this.ti_input.Text = "4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(4, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 28);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Энергия";
+            // 
+            // P_input
+            // 
+            this.P_input.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.P_input.Location = new System.Drawing.Point(80, 112);
+            this.P_input.Name = "P_input";
+            this.P_input.Size = new System.Drawing.Size(100, 36);
+            this.P_input.TabIndex = 2;
+            this.P_input.Text = "0.90";
+            // 
+            // wmax
+            // 
+            this.wmax.AutoSize = true;
+            this.wmax.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wmax.Location = new System.Drawing.Point(296, 25);
+            this.wmax.Name = "wmax";
+            this.wmax.Size = new System.Drawing.Size(0, 28);
+            this.wmax.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea1.AxisX.Crossing = 0D;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea1.AxisX.Title = "t, мкс";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea1.AxisY.Crossing = 0D;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Rotated270;
+            chartArea1.AxisY.Title = "S(t), В";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Alignment = System.Drawing.StringAlignment.Center;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            legend1.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(216, 76);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.SystemColors.InactiveCaption;
+            series1.Legend = "Legend1";
+            series1.Name = "Исходный сигнал";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Восстановленный сигнал";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.MarkerColor = System.Drawing.Color.Black;
+            series3.MarkerSize = 15;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Точки отсчётов";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(791, 350);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(14, 175);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 32);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Строить графики?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 532);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.wmax);
+            this.Controls.Add(this.P_input);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ti_input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Emax_input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculating);
+            this.Name = "Form1";
+            this.Text = "Lab3";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculating;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Emax_input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ti_input;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox P_input;
+        private System.Windows.Forms.Label wmax;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ chiftObr_Lab3/chiftObr_Lab3/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Forms;
+
+
+namespace chiftObr_Lab3
+{
+    public partial class Form1 : Form
+    {
+        double Emax, // Амплитуда
+            ti, // Длительность
+            w_verx, // Частотный диапазон
+            N, // Кол-во точек отсчета
+            dt, dw;
+        public Form1() { InitializeComponent(); }
+        void Calculating_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(Emax_input.Text, out Emax) || !double.TryParse(ti_input.Text, out ti)) { MessageBox.Show("Ошибка в входных данных"); return; }
+            ti *= Math.Pow(10, -6); ;
+            dt = ti / 100;
+            if (P_input.Text.Contains(".") && double.TryParse(P_input.Text.Replace('.', ','), out double P)) { }
+            else if (P_input.Text.Contains(",") && double.TryParse(P_input.Text, out P)) { }
+            else { MessageBox.Show("Ошибочный разделитель дробной части!"); return; }
+            chart1.Series[0].Points.Clear(); chart1.Series[1].Points.Clear(); chart1.Series[2].Points.Clear();
+            w_verx = (1.0 / ti);
+            double Ec = this.Ec(), Edw;
+            dw = w_verx / 100;
+            do
+            {
+                Edw = CalcEdw();
+                w_verx += (int)(1.0 / ti);
+            }
+            while (Edw / Ec < P);//Пока не достигнем точности в P% продолжаем расчет
+
+            double fd = w_verx / Math.PI;//Частота дискретизации
+            double delta_t = 1 / fd;//Расстояние между отсчётами
+            N = Math.Ceiling(ti / delta_t);
+
+            delta_t = ti / N;
+            fd = 1 / delta_t;
+            w_verx = Math.Ceiling(fd * Math.PI);
+            if (checkBox1.Checked)
+            {// Если отображение графиков включено
+                double h = ti / 128;//Шаг отрисовки точки
+                for (double t = 0; t <= ti; t += h)
+                {// Строим график S(t) на промежутке [ 0 ; ti ]
+                    chart1.Series[0].Points.AddXY(t * Math.Pow(10, 6), S(t));
+                    chart1.Series[1].Points.AddXY(t * Math.Pow(10, 6), CalcF(t));
+                }
+                for (int t = 0; t <= N; t++) chart1.Series[2].Points.AddXY(delta_t * t * Math.Pow(10, 6), S(delta_t * t));
+            }
+            wmax.Text = "Эффективная ширина спектра данного сигнала: " + w_verx/1000000 + " МГц";
+        }
+        double S(double t)
+        {// Функция исходного сигнала
+            if (t >0 && t <= (ti/2 )) return (-2*Emax*t)/ti;
+            else
+            if (t > (ti / 2)) return (2*Emax*(t-ti))/ti;
+            return 0;
+        }
+        Complex Sw(double w)
+        {// Спектральная плотность сигнала
+            Complex Sum = new Complex(0, 0);
+            for (double t = 0; t <= ti; t += dt)
+                Sum += S(t) * new Complex(Math.Cos(w * t), -Math.Sin(w * t)) * dt;
+            return Sum;
+        }
+        double Ec()
+        {// Полная энергия одиночного импульса
+            double E_c = 0;
+            for (double t = 0; t <= ti; t += dt)
+                E_c += Math.Pow(S(t), 2) * dt;
+            return E_c;
+        }
+        double CalcEdw()
+        {// Энергия, сосредоточенная в полосе частот
+            double Sum = 0;
+            for (double w = 0; w <= w_verx; w += dw)
+                Sum += Math.Pow(Sw(w).Magnitude, 2) * dw;
+            return (1 / Math.PI) * Sum;
+        }
+        Complex GetSk(double k)
+        {// Мнгновенное значение сигнала
+            Complex Sum = new Complex(0, 0);
+            for (double w = -w_verx; w <= w_verx; w += dw)
+                Sum += Sw(w) * new Complex(Math.Cos(k * Math.PI * w / w_verx), Math.Sin(k * Math.PI * w / w_verx)) * dw;
+            return (1 / (2 * Math.PI)) * Sum;
+        }
+        double CalcF(double t)
+        {// Восстановленный сигнал через ряд Котельникова
+            double Sum = 0;
+            for (double k = 0; k <= N; k++)
+                Sum += GetSk(k).Real * Math.Sin(w_verx * (t - (k * Math.PI / w_verx))) / (w_verx * (t - (k * Math.PI / w_verx)));
+            return Sum;
+        }
+    }
+}
